d57da945bba07c6a2aaf01fc8d8c459dcc407311
Shaneio <44534398+Shaneio@users.noreply.github.com>
2024-08-30 14:37:31 +0100

    Add i3 and feh to install script

--- install.sh
+++ install.sh
@@ -17,3 +17,19 @@ ln -sf "$HOME/.dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/.dotfiles/X11" "$HOME/.config/X11"
 
+
+
+########
+# i3   #
+########
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/.dotfiles/i3" "$HOME/.config/i3"
+
+
+########
+# feh  #
+########
+
+rm -rf "$HOME/.config/feh"
+ln -s "$HOME/.dotfiles/feh" "$HOME/.config/feh"
