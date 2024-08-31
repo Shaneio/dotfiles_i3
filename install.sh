3a3e1dee80955f768b0563214a539c1c003fd056
Shaneio <44534398+Shaneio@users.noreply.github.com>
2024-08-31 15:16:10 +0100

    Add Tmux & TmuxP
    Changes:
    
    - i3 config
      - Add scratchpad functionality with tmux
    - zsh scripts/scratchpad
      - Add ftmuxp and scratchpad functions
      - ftmuxp func creates a preset tmux session

--- install.sh
+++ install.sh
@@ -71,3 +71,15 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 #########
 
 ln -sf "$DOTFILES/gitconfig" "$XDG_CONFIG_HOME/gitconfig"
+
+
+#########
+# Tmux  #
+#########
+
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+
+[ ! -d "$XDG_CONFIG_HOME/tmux/plugins" ] \
+&& git clone https://github.com/tmux-plugins/tpm \
+"$XDG_CONFIG_HOME/tmux/plugins/tpm"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -48,8 +48,14 @@ then
     pgrep i3 || exec startx "$XDG_CONFIG_HOME/X11/.xinitrc"
 fi
 
-
-
 source $DOTFILES/zsh/scripts.sh
+
+ftmuxp
+
+# Clearing the shell is now done with CTRL+g
+bindkey -r '^l'
+bindkey -r '^g'
+bindkey -s '^g' 'clear\n'
+
 
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
